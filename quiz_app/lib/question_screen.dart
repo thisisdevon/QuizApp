c774abad867df2bc7744a5dd6bc7f911c2c17d25
devon-sulistio <devonstinson21japan@gmail.com>
2024-12-13 15:09:43 +0900

    Fix import

--- quiz_app/lib/question_screen.dart
+++ quiz_app/lib/question_screen.dart
@@ -14,7 +14,7 @@ class _QuestionScreenState extends State<QuestionScreen>
 {
   @override
   Widget build(BuildContext context) {
-    return Text("Question Screen");
+    return const Text("Question Screen");
   }
   
 }
--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/quiz.dart';
 
 class StartScreen extends StatelessWidget {
 
